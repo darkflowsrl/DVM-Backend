c6493ad6e3308d8a8b1b019a913c679ad65d4cf0
Giuliano Crenna <59456310+giulicrenna@users.noreply.github.com>
2024-01-23 11:35:53 -0300

    Update can_bus.py

--- src/can_bus.py
+++ src/can_bus.py
@@ -14,14 +14,73 @@ args = cli_parser.parse_args()
 buffer_len: int = 1000
 buffer: list = []
 
+class Message:
+    def __init__(self,
+                 hum: int,
+                 wind_dir: int,
+                 wind_speed: int,
+                 temp: int,
+                 dp: int) -> None:
+        self.hum = hum
+        self.wind_dir = wind_dir
+        self.wind_speed = wind_speed
+        self.temp = temp
+        self.dp = dp
+        
+    def parse_dict(self) -> dict:
+        return {
+            "humedad" : self.hum,
+            "velViento" : self.wind_speed,
+            "dirViento" : self.wind_dir,
+            "temperatura" : self.temp,
+            "puntoDeRocio" : self.dp
+        }
+
+class Parser:
+    def __init__(self, id: int, data: bytearray) -> None:
+        self.id = id
+        self.data = data
+        self.data_int = int.from_bytes(self.data, byteorder='little')
+        
+    def parse(self) -> str:
+        if self.id == 130313:
+            return "Humedad: " + str(self.data_int * 0.004)
+        if self.id == 130306:
+            speed: float = int.from_bytes(self.data[0:2], byteorder='little') * 0.01
+            dir: float = int.from_bytes(self.data[2:], byteorder='little') * 0.0001
+            
+            return f"Velocidad: {speed}\nDireccion: {dir}"
+        if self.id == 65269:
+            temp: float = self.data_int * 0.01 - 273.15
+            
+            return f"Temperatura: {temp}"
+        if self.id == 1000:
+            pr: float = self.data_int * 0.01 - 273.15
+            return f"Punto de rocio: {pr}"
+        """
+        if self.id == 129029:
+            return ""
+        if self.id == 129030:
+            return ""
+        if self.id == 129031:
+            return ""
+        if self.id == 129032:
+            return ""
+        """
+    
+    
 def load_message(msg: can.Message) -> None:
     if len(buffer) >= buffer_len:
         buffer.pop()
         buffer.append(msg)
-        print(f"{msg.arbitration_id}: {msg.data} : {int.from_bytes(msg.data, byteorder='little')}")
+        message_parser = Parser(msg.arbitration_id, msg.data)
+        
+        print(f"> {message_parser.parse()}")
     else:
         buffer.append(msg)
-        print(f"{msg.arbitration_id}: {msg.data} :  {int.from_bytes(msg.data, byteorder='little')}")
+        message_parser = Parser(msg.arbitration_id, msg.data)
+        
+        print(f"> {message_parser.parse()}")
 
 async def main() -> None:
     if args.mode == "read":
