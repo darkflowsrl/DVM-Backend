2d308704b37f9f09037a859feee2448532301886
Giuliano Crenna <59456310+giulicrenna@users.noreply.github.com>
2024-01-23 10:39:58 -0300

    Update can_bus.py

--- src/can_bus.py
+++ src/can_bus.py
@@ -18,10 +18,10 @@ def load_message(msg: can.Message) -> None:
     if len(buffer) >= buffer_len:
         buffer.pop()
         buffer.append(msg)
-        print(f"{msg.arbitration_id}: {msg.data}")
+        print(f"{msg.arbitration_id}: {msg.data} : {msg.data.decode('utf-8')}")
     else:
         buffer.append(msg)
-        print(f"{msg.arbitration_id}: {msg.data}")
+        print(f"{msg.arbitration_id}: {msg.data} :  {msg.data.decode('utf-8')}")
 
 async def main() -> None:
     if args.mode == "read":
