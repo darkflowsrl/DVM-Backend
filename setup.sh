00fadb609b35a515be04eff8029d138d253da616
Giuliano Crenna <59456310+giulicrenna@users.noreply.github.com>
2024-02-19 17:25:55 -0300

    modified:   setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-apt install -y python3-pip
+apt install -y dist-upgrade
+apt install -y python3-pip --fix-missing
 apt install -y htop
 apt install -y nodm
 
@@ -9,9 +10,12 @@ apt install -y nodm
 # NODM_ENABLED=true
 # NODM_USER=root
 
+# nano /etc/systemd/system/getty.target.wants/getty\@tty1.service
+# exec: /sbin/getty --autologin ubuntu -8 38400 tty1
+
 echo "sleep 10" >> ~/.bashrc
-echo "nohup python3 /root/Darkflow-HMI-Backend/main.py > /dev/null 2>1& &" >> ~/.bashrc
-echo "startx /root/Darkflow-HMI-Backend/dvm-app-front-1.0.8.AppImage -- -nocursor " >> ~/.bashrc
+echo "nohup python3 /root/Darkflow-HMI-Backend/main.py > /dev/null 2>1&" >> ~/.bashrc
+echo "startx /root/Darkflow-HMI-Backend/dvm-app-front-1.0.7.AppImage --no-sandbox -- -nocursor" >> ~/.bashrc
 echo "ip link set can0 up type can bitrate 250000" >> ~/.bashrc
 echo "ip link set can0 up type can bitrate 250000" >> ~/.bashrc
 
@@ -23,7 +27,7 @@ pip3 install -r requirements.txt
 
 cd Darkflow-HMI-Backend
 
-wget https://github.com/SegarraFacundo/DVM-front/releases/download/v1.0.8/dvm-app-front-1.0.8.AppImage
+wget https://github.com/SegarraFacundo/DVM-front/releases/download/v1.0.8/dvm-app-front-1.0.7.AppImage
 
 systemctl set-default multi-user.target
 
