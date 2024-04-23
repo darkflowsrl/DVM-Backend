7e9e7d59de34667c1ba68a4e052db4281383fcf0
Giuliano Crenna <59456310+giulicrenna@users.noreply.github.com>
2024-04-23 13:09:33 -0300

    Update log.py

--- src/log.py
+++ src/log.py
@@ -1,36 +1,39 @@
-import os 
+import os
 import datetime
+import csv
 
-LOG_PATH: str = os.path.join(os.getcwd(), 'log.txt')
+LOG_DIR: str = os.path.join(os.getcwd(), 'logs')
 
-def _check_log_file(MAX: int = 10) -> None:
-    if not os.path.isfile(LOG_PATH):
-        # Create file if not exist
-        open(LOG_PATH, 'x')
+def _check_log_file() -> None:
+    if not os.path.isdir(LOG_DIR):
+        os.makedirs(LOG_DIR)
 
-        return 
-    
-    # Rewrite file if is over a specified size
-    size: int = os.path.getsize(LOG_PATH)
-    if size//1000000 >= MAX: open(LOG_PATH, "w").close()
+def get_log_path() -> str:
+    today_date = datetime.date.today()
+    log_filename = f"log_{today_date}.csv"
+    return os.path.join(LOG_DIR, log_filename)
 
 def log(message: str, instance: any = 'Unknow Instance') -> None:
     try:
         _check_log_file()
         
+        log_path = get_log_path()
+        
         now: str = str(datetime.datetime.now())
         now = now.split('.')[0]
 
-        message = f'[{now}] {instance} -> {message}\n'
+        if not os.path.isfile(log_path):
+            with open(log_path, 'w', newline='') as csvfile:
+                fieldnames = ['Timestamp', 'Instance', 'Message']
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                writer.writeheader()
         
-        with open(LOG_PATH, 'a') as f:
-            f.write(message)
-            f.close()
+        with open(log_path, 'a', newline='') as csvfile:
+            fieldnames = ['Timestamp', 'Instance', 'Message']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+            writer.writerow({'Timestamp': now, 'Instance': instance, 'Message': message})
 
     except Exception as e:
         print(e)
         raise e
-    
-if __name__ == '__main__':
-    for i in range(100):
-        log(i, __name__)
