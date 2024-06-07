aee0220e616df6722db6679f6bc962d1f7a77abc
Giuliano Crenna <59456310+giulicrenna@users.noreply.github.com>
2024-06-07 12:39:33 -0300

    Update download-front.sh

--- download-front.sh
+++ download-front.sh
@@ -5,3 +5,5 @@ VERSION="$1"
 cd /root
 
 wget https://github.com/SegarraFacundo/DVM-front/releases/download/v$VERSION/dvm-app-front-$VERSION.AppImage
+
+chmod +x /root/dvm-app-front-$VERSION.AppImage
