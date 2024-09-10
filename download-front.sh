73777d6ae0e4e05e642aa843a5f4dd04456e2d01
Giuliano Crenna <59456310+giulicrenna@users.noreply.github.com>
2024-09-10 17:04:33 -0300

    Se borraron archivos innecesarios

--- download-front.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-VERSION="$1"
-
-cd /root
-
-wget https://github.com/SegarraFacundo/DVM-front/releases/download/v$VERSION/dvm-app-front-$VERSION.AppImage
-
-chmod +x /root/dvm-app-front-$VERSION.AppImage
--- enable_gui.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sudo systemctl set-default graphical.target
-sudo reboot
